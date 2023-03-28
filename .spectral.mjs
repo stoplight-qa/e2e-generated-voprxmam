4d28ae81c2ceb7f3c687786b86e0c977298e7d40
qa-qgrmu-1-amxk@example.com <qa-qgrmu-1-amxk@example.com>
2023-03-28 12:17:32 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-15836.stoplight-dev.com/api/v1/projects/cHJqOjQwMA/spectral.js?branch_slug=main&token=508c188a-66a1-4497-b3f8-939f7967205b";
+export default { extends: ruleset };
